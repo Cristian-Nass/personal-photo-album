ced2a193684a80b3af7e244d13e58f2bd02b05bf
Cristian <cristian.tirdad@gmail.com>
2021-11-04 15:05:54 +0100

    Implemented Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import React, { useEffect } from "react";
-import "./App.css";
-import { db } from "./service/firebase";
-import { collection, query, getDocs } from "firebase/firestore";
+import React, {useEffect} from 'react';
+import './App.css';
+import {db} from './service/firebase';
+import {collection, query, getDocs} from 'firebase/firestore';
 const getData = async (q: any) => {
   const querySnapshot = await getDocs(q);
   querySnapshot.forEach((doc) => {
     // doc.data() is never undefined for query doc snapshots
-    console.log(doc.id, " => ", doc.data());
+    console.log(doc.id, ' => ', doc.data());
   });
 };
 function App() {
   useEffect(() => {
-    const q = query(collection(db, "users"));
+    const q = query(collection(db, 'users'));
     getData(q);
   }, []);
 
-  return <div className='App'>Wellconme to Personal Photo Album</div>;
+  return <div className="App">Wellconme to Personal Photo Album</div>;
 }
 
 export default App;
