5d30add2fa715e817137925093375d29daed0030
Cristian <cristian.tirdad@gmail.com>
2021-11-04 14:45:09 +0100

    connect to firebase emulator

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-
+import { db } from "./service/firebase";
+import { collection, query, getDocs } from "firebase/firestore";
+const getData = async (q: any) => {
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+    console.log(doc.id, " => ", doc.data());
+  });
+};
 function App() {
+  useEffect(() => {
+    const q = query(collection(db, "users"));
+    getData(q);
+  }, []);
+
   return <div className='App'>Wellconme to Personal Photo Album</div>;
 }
 
